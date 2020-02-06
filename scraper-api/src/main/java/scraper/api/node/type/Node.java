186b85c5d57d0d904f45be6de3d9229bc15256d4
meides <meides@sero-systems.de>
2020-02-06 17:12:17 +0100

    Added code to Node for sending debug data

--- scraper-api/src/main/java/scraper/api/node/type/Node.java
+++ scraper-api/src/main/java/scraper/api/node/type/Node.java
@@ -10,6 +10,12 @@ import scraper.api.node.NodeHook;
 import scraper.api.node.container.NodeLogLevel;
 import scraper.api.specification.ScrapeInstance;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 /**
  * Objects which implement this interface can consume and modify {@link FlowMap}s.
  *
@@ -30,6 +36,7 @@ public interface Node {
     default @NotNull FlowMap accept(@NotNull final NodeContainer<? extends Node> n, @NotNull final FlowMap o) throws NodeException {
         try {
             for (NodeHook hook : n.beforeHooks()) { hook.accept(o); }
+            this.sendDebugData(n, o);
             FlowMap fm = process(n, o);
             for (NodeHook hook : n.afterHooks()) { hook.accept(o); }
             return fm;
@@ -39,9 +46,50 @@ public interface Node {
         }
     }
 
+    default void sendDebugData(NodeContainer<? extends Node> n, FlowMap o) {
+        DebugData d = new DebugData(n, o);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8500"))
+                .POST(HttpRequest.BodyPublishers.ofString(d.toString()))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     /** The process function which encapsulates the business logic of a node */
     @NotNull FlowMap process(@NotNull NodeContainer<? extends Node> n, @NotNull FlowMap o) throws NodeException;
 
     /** Initialization of a node, by default no-op */
     default void init(@NotNull NodeContainer<? extends Node> n, @NotNull ScrapeInstance instance) throws ValidationException {}
+}
+
+class DebugData {
+    private NodeContainer<? extends Node> n;
+    private FlowMap o;
+
+    public DebugData(NodeContainer<? extends Node> n, FlowMap o) {
+        this.n = n;
+        this.o = o;
+    }
+
+    @Override
+    public String toString() {
+//        return super.toString();
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Java object to JSON string
+        String jsonString = mapper.writeValueAsString(object);
+
+        return jsonString;
+    }
 }
