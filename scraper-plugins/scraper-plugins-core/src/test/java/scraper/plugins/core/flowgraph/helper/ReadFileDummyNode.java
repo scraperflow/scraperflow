ae5f39d8083df5b528fbc6a8bd960e1a9ad9ad20
Albert Schimpf <git@schimpf.link>
2020-06-05 19:30:22 +0200

    Replaced dummy node

---
+++ scraper-plugins/scraper-plugins-core/src/test/java/scraper/plugins/core/flowgraph/helper/ReadFileDummyNode.java
@@ -0,0 +1,32 @@
+package scraper.plugins.core.flowgraph.helper;
+
+import scraper.annotations.NotNull;
+import scraper.annotations.node.FlowKey;
+import scraper.annotations.node.NodePlugin;
+import scraper.api.exceptions.NodeException;
+import scraper.api.flow.FlowMap;
+import scraper.api.node.container.FunctionalNodeContainer;
+import scraper.api.node.type.FunctionalNode;
+import scraper.api.template.L;
+import scraper.api.template.T;
+
+/**
+ * @author Albert Schimpf
+ */
+@NodePlugin(value = "0.0.1")
+public final class ReadFileDummyNode implements FunctionalNode {
+
+    @FlowKey
+    private T<String> inputFile = new T<>(){};
+
+    @FlowKey
+    private L<String> output = new L<>(){};
+
+    @FlowKey
+    private String charset;
+
+    @Override
+    public void modify(@NotNull FunctionalNodeContainer n, @NotNull FlowMap o) throws NodeException {
+        // do nothing as pure as it gets
+    }
+}
